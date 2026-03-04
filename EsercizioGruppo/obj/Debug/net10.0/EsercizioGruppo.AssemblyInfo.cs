e63109b65795b63e32e5d53d06c60e7eeceedbc3
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-03-04 17:42:49 +0100

    Aggiornato gitignore

--- EsercizioGruppo/obj/Debug/net10.0/EsercizioGruppo.AssemblyInfo.cs
+++ EsercizioGruppo/obj/Debug/net10.0/EsercizioGruppo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65d2c9c74c78ecf5d243d64565fd825a07d4290")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664c2336229c02c625b0d2ee68de1aa8dc4eb6c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
