f6033a102323950fda74f7915425aacbfa97978b
fcard-dev <fabrizia.cardinale@gmail.com>
2026-03-04 15:47:41 +0100

    git ignore

--- EsercizioGruppo/obj/Debug/net10.0/EsercizioGruppo.AssemblyInfo.cs
+++ EsercizioGruppo/obj/Debug/net10.0/EsercizioGruppo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a25efa9336838565d6007753b3465494d246515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763f11f89091f3657635a8e21d4359ae2e9644d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsercizioGruppo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
