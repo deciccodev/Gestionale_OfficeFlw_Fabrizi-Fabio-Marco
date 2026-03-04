a8cf0e5c7c501135cbfc474744ef48876d5d557d
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-03-04 15:28:19 +0100

    Correzione classe Operazione

---
+++ EsercizioGruppo/Operazione.cs
@@ -0,0 +1,53 @@
+abstract class Operazione
+{
+    private int id;
+    private int clienteId;
+    private DateTime data;
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+        set
+        {
+            if(value > 0)
+                id = value;
+            else
+                Console.WriteLine("Valore id non valido.");
+        }
+    }
+
+    public int ClienteId
+    {
+        get
+        {
+            return clienteId;
+        }
+        set
+        {
+            if(value > 0)
+                clienteId = value;
+            else
+                Console.WriteLine("Valore id non valido.");
+        }
+    }
+
+    public DateTime Data
+    {
+        get
+        {
+            return data;
+        }
+        set
+        {
+            if(value < DateTime.Now)
+                data = value;
+            else
+                Console.WriteLine("Data non valida.");
+        }
+    }
+
+    public abstract double CalcolaTotale();
+}
